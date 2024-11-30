f0c61da3ce540c9173d2a7e8421cfeaff4d0eb36
Christian <naresan50@hotmail.com>
2024-11-30 02:02:47 -0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/**@type {import { 'next' }.Nextconfig} */
+const nextconfig = {
+  env: {
+    API_URL: "https://proyecto-app-nu.vercel.app/api/chat/"
+   }
+};
+export default nextconfig;
---
+++ src/components/MessageList.jsx
@@ -0,0 +1,12 @@
+export default function MessageList({ messages }) {
+    return (
+      <div className="list-group">
+  {messages?.map((message) => (
+    <div key={message.id} className="message-container">
+      <strong>{message.author.name}:</strong> {message.content}
+    </div>
+  ))}
+</div>
+
+    );
+  }
